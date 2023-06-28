167a4bbfca2abbaf0fcf71cd34c902a517b67c59
dkak25 <31278874+dkak25@users.noreply.github.com>
2023-06-28 11:53:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,22 @@ streamlit.text('😎Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#####################################################
 # building fruit list table and multiselect
+#####################################################
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+#####################################################
 # Let's put a pick list here so they can pick the fruit they want to include 
+#####################################################
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+#####################################################
 # New Section to display fruityvice api response
+#####################################################
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
@@ -28,9 +34,14 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
+#####################################################
 # normalize the json
+#####################################################
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+#####################################################
 # shows it as a dataframe
+#####################################################
 streamlit.dataframe(fruityvice_normalized)
 
 
@@ -39,12 +50,13 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit list contains:")
-streamlit.text(my_data_row)
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit list contains:")
+streamlit.dataframe(my_data_rows)
 
-
+#####################################################
 # Allow the end user to add a fruit to the list
+#####################################################
 streamlit.header("What rfuit would you like to add?")
 add_my_fruit = streamlit.text_input('What rfuit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
